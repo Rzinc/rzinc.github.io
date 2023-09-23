d5c9499158c5b793dbe4ce81c5a8e4557e80d0b9
Rahul V <rahul.vinod2011@gmail.com>
2023-09-23 17:19:47 +0530

    mouse interactive circles

--- Interaction/canvas.js
+++ Interaction/canvas.js
@@ -1,15 +1,46 @@
 // Creating multiple circles using object oriented JS
-
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 var canvas = document.querySelector("canvas");
 
-winHeight = window.innerHeight-2
-winWidth = window.innerWidth-2 
-canvas.width = winWidth;
-canvas.height = winHeight;
+function setWindow(){
+    winHeight = window.innerHeight-2
+    winWidth = window.innerWidth-2 
+    canvas.width = winWidth;
+    canvas.height = winHeight;
+}
+setWindow()
+
+var minRadius = 7;
+var maxRadius = 40;
+var colorArray = [
+    '#ffaaff',
+    '#faaaaa',
+    '#123456',
+    '#134569'
+]
 
 var c = canvas.getContext('2d')
 
+var mouse = {
+    x : undefined,
+    y : undefined
+}
+
+
+//Creating an event listener
+window.addEventListener('mousemove', function(event) { //this anonymous function aalways has an event argument
+    mouse.x = event.x;
+    mouse.y = event.y;
+})
+
+window.addEventListener('resize', function(event){
+    setWindow();
+
+    init();
+})
 
 //Creating a JS Object, make a function with a name starting off with a capital letter
 function Circle(x, y, dx, dy, radius){
@@ -19,15 +50,19 @@ function Circle(x, y, dx, dy, radius){
     this.dx = dx;
     this.dy = dy;
     this.radius = radius
+    this.color = colorArray[Math.floor(Math.random()*colorArray.length)]
+    this.minRadius = radius;
 
     //The circle will be drawn whenever this is called
-    this.draw = function(){
+    this.draw = function(){ 
         c.beginPath();
-        c.strokeStyle = "pink"
-        c.arc(this.x, this.y, 30, 0, 2*Math.PI, false);
-        c.stroke()
+        // c.strokeStyle = "pink"
+        c.arc(this.x, this.y, this.radius, 0, 2*Math.PI, false);
+        c.fillStyle = this.color;
+        // c.stroke()
+        c.fill()
     }
-    //Make sure to use this to ensure that the circle variables are referenced instead o fother random variables
+    //Make sure to use this to ensure that the circle variables are referenced instead of other random variables
     this.update = function(){
         if (this.x >= winWidth-radius || this.x <= radius)
             {
@@ -39,33 +74,41 @@ function Circle(x, y, dx, dy, radius){
                 this.dy*=-1
             }
     
-            this.x+=this.dx;
-            this.y+=this.dy;
-            
-            this.draw();
+        this.x+=this.dx;
+        this.y+=this.dy;
+        //Interactivity
+        if ((Math.abs(mouse.x - this.x) < 50 && Math.abs(mouse.y - this.y) < maxRadius) && this.radius <= 40) 
+        {
+            this.radius += 1;
+        }
+        else if(this.radius>= this.minRadius)
+        {
+            this.radius -= 1;
+        }
+        this.draw();
     }
 }
 
 
+function init() {
+    //JS Array
+    circleArray = [] //Variables declared wihtout var become global variables
 
-//JS Array
-circleArray = []
+    for(var i  = 0; i != 1000; ++i){
 
-for(var i  = 0; i != 100; ++i){
+        var radius = Math.random()*3+1;
+        // Math.random() generates a number b/w 0 and 1
+        var x = Math.random()*(winWidth - radius*2) + radius;
+        var y = Math.random()*(winHeight - radius*2) + radius;
+        var dx = (Math.random()-0.5)*4
+        var dy = (Math.random()-0.5)*4
 
-    var radius = 30
-    // Math.random() generates a number b/w 0 and 1
-    var x = Math.random()*(winWidth - radius*2) + radius;
-    var y = Math.random()*(winHeigh - radius*2) + radiust;
-    var dx = (Math.random()-0.5)*4
-    var dy = (Math.random()-0.5)*4
+        //Instantiating an object in JS
+        circleArray.push(new Circle(x, y, dx, dy, radius));
 
-    //Instantiating an object in JS
-    circleArray.push(new Circle(x, y, dx, dy, radius));
 
-    console.log(circleArray[i]);
+    }
 }
-
 function animate() {
     requestAnimationFrame(animate);
     //This will create a loop for us
@@ -77,5 +120,5 @@ function animate() {
     }
 }
 
-
+init();
 animate();
