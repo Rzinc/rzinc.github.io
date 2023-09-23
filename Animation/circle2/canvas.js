f8656746bf304dbf9b69a426a0c283dc830e8f4e
Rahul V <rahul.vinod2011@gmail.com>
2023-09-23 16:01:14 +0530

    many circles

--- Animation/circle2/canvas.js
+++ Animation/circle2/canvas.js
@@ -1,3 +1,6 @@
+// Creating multiple circles using object oriented JS
+
+
 var canvas = document.querySelector("canvas");
 
 winHeight = window.innerHeight-2
@@ -7,35 +10,71 @@ canvas.height = winHeight;
 
 var c = canvas.getContext('2d')
 
-var radius = 30
-// Math.random() generates a number b/w 0 and 1
-var x = Math.random()*winWidth;
-var y = Math.random()*winHeight;
-var dx = (Math.random()-0.5)*4
-var dy = (Math.random()-0.5)*4
-console.log(x, y, dx, dy);
+
+//Creating a JS Object, make a function with a name starting off with a capital letter
+function Circle(x, y, dx, dy, radius){
+    //To give each circle it's own x and y value
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius
+
+    //The circle will be drawn whenever this is called
+    this.draw = function(){
+        c.beginPath();
+        c.strokeStyle = "pink"
+        c.arc(this.x, this.y, 30, 0, 2*Math.PI, false);
+        c.stroke()
+    }
+    //Make sure to use this to ensure that the circle variables are referenced instead o fother random variables
+    this.update = function(){
+        if (this.x >= winWidth-radius || this.x <= radius)
+            {
+                this.dx*=-1
+            }
+    
+            if (this.y >= winHeight-radius || this.y <= radius)
+            {
+                this.dy*=-1
+            }
+    
+            this.x+=this.dx;
+            this.y+=this.dy;
+            
+            this.draw();
+    }
+}
+
+
+
+//JS Array
+circleArray = []
+
+for(var i  = 0; i != 100; ++i){
+
+    var radius = 30
+    // Math.random() generates a number b/w 0 and 1
+    var x = Math.random()*(winWidth - radius*2) + radius;
+    var y = Math.random()*(winHeigh - radius*2) + radiust;
+    var dx = (Math.random()-0.5)*4
+    var dy = (Math.random()-0.5)*4
+
+    //Instantiating an object in JS
+    circleArray.push(new Circle(x, y, dx, dy, radius));
+
+    console.log(circleArray[i]);
+}
+
 function animate() {
     requestAnimationFrame(animate);
     //This will create a loop for us
     //circle
     c.clearRect(0, 0 ,winWidth, winHeight);
-    c.beginPath();
-    c.strokeStyle = "pink"
-    c.arc(x, y, radius, 0, 2*Math.PI, false);
-    c.stroke()
-
-    if (x >= winWidth-radius || x <= radius)
-    {
-        dx*=-1
+    for (var i = 0; i != circleArray.length; ++i){
+            //The new function circle
+            circleArray[i].update();
     }
-
-    if (y >= winHeight-radius || y <= radius)
-    {
-        dy*=-1
-    }
-
-    x+=dx;
-    y+=dy;
 }
 
 
